a5f821f294fd366bd67ec05037abcc0aff3afac8
PitToYondeKudasai <buttaronipeter@gmail.com>
2021-02-17 12:01:51 +0100

    Update SpaceInvaders.cpp

--- SpaceInvaders.cpp
+++ SpaceInvaders.cpp
@@ -1,14 +1,17 @@
 #include <curses.h>
 #include <vector>
+#include <string>
 
 #define WALL '#'
 #define PLAYER '='
 #define LASER '*'
+#define ENEMY 'O' 
 #define EMPTY ' '
 
 bool gameOver;
 int x, y;
 int x_old, y_old;
+int score;
 
 const int height = 20, width = 30;
 
@@ -21,22 +24,51 @@ void draw_window(int x_pos, int y_pos){
 class laser_beam{
     public:
         int x_laser = 0, y_laser = 0;
+        bool hit = false;
 
         void update(){
             if((y_laser - 1) > 1){
                 y_laser -= 1;
             } else {
-                y_laser = 100;
+                y_laser = -10;
             }
+        }
+};
+
+class enemy{
+    public:
+        int x_enemy, y_enemy;
+        bool right = true;
+        bool destroyed = false;
+
+        void update(){
+            /*
+            if (right) {
+                x_enemy += 1;
+                right = false;
+            } else {
+                x_enemy -= 1;
+                right = true;
+            }
+            */
         }
 };
 
 class Character{
     private:
         std::vector<laser_beam> lasers;
+        std::vector<enemy> enemies;
 
-        void update_by_ref(int & y_laser){
-            y_laser -= 1;
+        void update_by_ref(laser_beam & las, bool hit){
+            if(hit){
+                las.y_laser = -10;
+            } else {
+                las.y_laser -= 1;
+            }
+        }
+
+        void destroy_by_ref(bool & destroyed){
+            destroyed = true;
         }
     
         void move_right(){
@@ -60,7 +92,31 @@ class Character{
             lasers.push_back(las);
         }
 
+        void hit_enemy(laser_beam & las){
+            for(int i = 0; i < enemies.size(); i++){
+                if(!enemies[i].destroyed){
+                    if((enemies[i].x_enemy == las.x_laser) && (enemies[i].y_enemy == (las.y_laser + 1))){
+                        destroy_by_ref(enemies[i].destroyed);
+                        update_by_ref(las, true);
+                        score += 1;
+                    }
+                }
+            }
+        }
+
     public:
+        void initialise_enemies(){
+            for(int i = 3; i < 27; i++){
+                enemy e;
+                e.x_enemy = i;
+                e.y_enemy = 3;
+                enemies.push_back(e);
+                e.x_enemy = i;
+                e.y_enemy = 4;
+                enemies.push_back(e);
+            }
+        }
+
         void draw(){
             mvaddch(y, x, PLAYER);
             mvaddch(y_old, x_old, EMPTY);
@@ -68,16 +124,26 @@ class Character{
             for(laser_beam las : lasers){
                 if(las.y_laser != 0)
                     mvaddch(las.y_laser, las.x_laser, LASER);
-                if((las.y_laser + 1) != 0)
+                if(las.y_laser + 1 != 0)
                     mvaddch((las.y_laser + 1), las.x_laser, EMPTY);
             }
 
+            for(enemy en : enemies){
+                if(en.destroyed){
+                    mvaddch(en.y_enemy, en.x_enemy, EMPTY);
+                } else {
+                    mvaddch(en.y_enemy, en.x_enemy, ENEMY);
+                }
+            }
+            
+            mvprintw(22, 15, "%d",score);
             move(y, x);
         }
 
         void update(){
             for(int i = 0; i < lasers.size(); i++){
-                update_by_ref(lasers[i].y_laser);
+                update_by_ref(lasers[i], false);
+                hit_enemy(lasers[i]);
             }
 
             int ch;
@@ -99,22 +165,6 @@ class Character{
         }
 };
 
-class Enemy{
-    int x_enemy, y_enemy;
-    bool right = true;
-
-    public:
-        void update(){
-            if (right) {
-                x_enemy += 1;
-                right = false;
-            } else {
-                x_enemy -= 1;
-                right = true;
-            }
-        }
-};
-
 int main(){
     // Initialise cursers
     initscr();
@@ -126,7 +176,9 @@ int main(){
     y = 20;
     x_old = x - 1;
     y_old = y;
+    score = 0;
     Character spaceship;
+    spaceship.initialise_enemies();
 
     draw_window(0, 0);
     spaceship.draw();
